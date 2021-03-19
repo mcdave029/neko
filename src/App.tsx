da808f4ebefe6b7b3d12d1d0b6f2bffed42d4118
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2021-03-19 18:49:00 +0800

    feat(node-sass): install node-sass and convert css files to scss
    import bootstrap in app.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
