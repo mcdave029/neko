0e6cab9ad524b6eb9e9124914b5ef0e79dc5ff80
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2021-03-19 23:55:11 +0800

    feat(axios): implement breeds, cats and load more functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,72 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 
 interface Breed {
   id: string;
   name: string;
+  origin: string;
+  description: string;
+  temperament: string;
+}
+
+interface CatDetails {
+  id: string;
+  url: string;
+  breeds: Array<Breed>
 }
 
 interface Props {}
 
 interface State {
   breeds: Array<Breed>
+  selectedBreed: string
+  cats: Array<CatDetails>,
+  page: number,
+  isLoading: boolean,
+  canLoadMore: boolean
 }
 
 class App extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      breeds: []
+      breeds: [],
+      selectedBreed: 'DEFAULT',
+      cats: [],
+      page: 0,
+      isLoading: true,
+      canLoadMore: true
     };
+  }
+  populateCats = (breed_id: string) => {
+    this.setState(
+      { selectedBreed: breed_id, page: this.state.page + 1 },
+      () => {
+        const options = {
+          headers: { 'x-api-key': process.env.REACT_APP_CAT_API_KEY },
+          params: { breed_id: this.state.selectedBreed, order: 'asc', limit: 5, page: this.state.page }
+        };
+        console.log(options);
+        axios.get('https://api.thecatapi.com/v1/images/search', options)
+          .then((response) => {
+            this.setState({ cats: [...this.state.cats, ...response.data], canLoadMore: response.data.length !== 0 });
+          });
+      }
+    );
+  }
+  onSelectBreed = (event: React.ChangeEvent<any>) => {
+    const breed_id = event.target.value;
+    this.setState(
+      { cats: [], page: 0 },
+      () => {
+        this.populateCats(breed_id);
+      }
+    );
+  }
+  onClick = (breed: Breed) => {
+    console.log(breed);
   }
   render() {
     return (
@@ -37,7 +85,7 @@ class App extends Component<Props, State> {
               <Form>
                 <Form.Group controlId="formBasicEmail">
                   <Form.Label>Breed</Form.Label>
-                  <Form.Control as="select" custom defaultValue={ 'DEFAULT' }>
+                  <Form.Control as="select" custom onChange={this.onSelectBreed} value={ this.state.selectedBreed}>
                     <option value="DEFAULT" disabled>Select breed</option>
                     {this.state.breeds.map((breed: Breed) => (
                       <option key={breed.id} value={breed.id}>{breed.name}</option>
@@ -48,11 +96,30 @@ class App extends Component<Props, State> {
             </Col>
           </Row>
           <Row>
-            <Col>No cats available</Col>
+            {this.state.cats.map((cat: CatDetails) => (
+            <Col md={{ span: 3 }} sm={{ span: 6}} key={cat.id}>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={cat.url} />
+                <Card.Body>
+                  <Button variant="primary" block onClick={() => this.onClick(cat.breeds[0])}>View details</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            ))}
           </Row>
+          { this.state.canLoadMore ?
           <Row>
-            <Col><Button variant="success">Load more</Button>{' '}</Col>
-          </Row>
+            <Col>
+              <Button
+                variant="success"
+                onClick={() => this.populateCats(this.state.selectedBreed) }
+                disabled={this.state.isLoading || this.state.cats.length === 0}
+              >
+                {this.state.isLoading ? 'Loading cats…' : 'Load more'}
+              </Button>{' '}
+            </Col>
+          </Row> : null
+          }
         </Container>
       </div>
     );
@@ -64,7 +131,7 @@ class App extends Component<Props, State> {
     };
     axios.get('https://api.thecatapi.com/v1/breeds', options)
       .then((response) => {
-        this.setState({ breeds: response.data });
+        this.setState({ breeds: response.data, isLoading: false });
       });
   }
 }
