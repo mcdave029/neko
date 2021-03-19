d90d21a797b76b6b411916a20848bea15fe21387
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2021-03-19 18:37:19 +0800

    feat(react-boostrap): install react-boostrap and incorporate it in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col><h1>Cat Browser</h1></Col>
+        </Row>
+        <Row>
+          <Col md={{ span: 3 }} sm={{ span: 6 }}>
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Breed</Form.Label>
+                <Form.Control as="select" custom>
+                  <option disabled selected>Select breed</option>
+                </Form.Control>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col>No cats available</Col>
+        </Row>
+        <Row>
+          <Col><Button variant="success">Load more</Button>{' '}</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
